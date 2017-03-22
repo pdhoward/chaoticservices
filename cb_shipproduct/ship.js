c548b309464f56386d38edc253ff8dacb563fa5e
pdhoward <patrick.howard@hotmail.com>
2017-03-22 15:24:38 -0400

    updates

--- cb_shipproduct/ship.js
+++ cb_shipproduct/ship.js
@@ -6,27 +6,24 @@
 function main(args) {
   console.log("SHIPPING FUNCTION")
   var text = args.text;
-
     return new Promise (function(resolve, reject){
       respond(text, function(response) {
           var result = {};
-          result.sender = 'Shipper';
-          result.intent = intent;
+          result.sender = 'Shipper';          
           result.receiver = undefined;
           result.callback = false;
           result.restart = false;
           result.redirect = false;
-          result.orgmessage = undefined;
-          result.handle = undefined;
+          result.orgmessage = undefined;          
           result.text = response;
           resolve(result)
       })
     })
   };
 
-  //classify returns an array of intents with confidence intervals.
+  //respond returns a string
   function respond(text, cb) {
-     var response = 'Hi I am the Shipping Agent. I can handle your request'
+     var response = 'Hi I am the Shipping Agent. How may I help you?'
      cb(response)
   };
 
