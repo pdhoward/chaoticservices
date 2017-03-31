ef4b7ce68996701789ab85f3dc7bed82f1c9303c
pdhoward <patrick.howard@hotmail.com>
2017-03-31 12:03:35 -0400

    updates

--- cb_proof/proof.js
+++ cb_proof/proof.js
@@ -14,13 +14,21 @@ const triggerWords = [
   'please',
   'absolutely',
   'ok',
-  'do it'
+  'do it',
+  'Yes',
+  'Please',
+  'Absolutely',
+  'Ok',
+  'Go',
+  'Do'
 ]
 
 var visit = {
   isReturn: false,
   isKnown: false,
   isTrigger: false,
+  isProof: false,
+  activeProof: false,
   count: 0
 }
 
@@ -31,9 +39,22 @@ var replyObj = {
   restart: false
 }
 
+var proofAgent = {
+  live: '+19145271623'
+}
+
 
 function main(args) {
   console.log("PROOF FUNCTION")
+
+  Object.getOwnPropertyNames(args).forEach(
+        function (val, idx, array) {
+            if ((val !== 'system') && (val !== 'handler')) {
+                  console.log(val + ' -> ' + args[val]); }
+            if (val == 'context') {
+                  console.log(val + ' -> ' + JSON.stringify(args[val])); }
+                }
+            );
 
     ////////////////////////////////////////////////////
     //////  Test Session Variable: New discussion?  ////
@@ -48,6 +69,24 @@ function main(args) {
       visit.isReturn = true,
       visit.count = req.session.count
        }
+// set for request for active session
+    if (args.context) {
+        if (args.context.proof) {
+          visit.isProof = true
+        } else {
+          visit.isProof = false
+        }
+      }
+// set for active session
+    if (args.context) {
+        if (args.context.activeProof) {
+            visit.activeProof = true
+        } else {
+          visit.activeProof = false
+        }
+      }
+// record number of sender
+    visit.sender = args.sender
 
     // test to see if sender is registered to platform
     var client = clients.find(function( item ) {
@@ -73,13 +112,14 @@ function main(args) {
 
   // compose response and determine callback
   return new Promise (function(resolve, reject){
-      respond(text, function(response) {
+      respond(args, function(response) {
           var result = {};
           result.sender = 'proof';
           result.receiver = undefined;
           result.callback = false;
           result.restart = false;
           result.redirect = false;
+          result.proof = false;
           result.orgmessage = undefined;
           result = Object.assign(result, response)
           resolve(result)
@@ -88,7 +128,7 @@ function main(args) {
   };
 
   //respond returns a string
-  function respond(text, cb) {
+  function respond(args, cb) {
 //     var response = 'Hi I am your sales rep. How may I help you?'
 //     cb(response)
     if (visit.isKnown) {
@@ -97,10 +137,42 @@ function main(args) {
       var extraText = ' new guy?';
     }
 
-    if (visit.isTrigger) {
-      replyObj.text = 'Go ahead and ask your questions. Your party is now online and ready';
+    if (visit.isProof) {
+      replyObj.text = args.text;  // pass through text from sender
       replyObj.redirect = false;
-      replyObj.callback = false;
+      replyObj.callback = true;
+      replyObj.proof = false;     // proof set to true up on trigger. Now that exchange has started ...
+      replyObj.activeProof = true; // ..... set to true to indicate that active exchange has commenced
+      replyObj.saveProofSender = visit.sender; // save sender for logic to toogle interchange
+      replyObj.saveProofAgent = proofAgent.live;  // agent selected for interchange
+      replyObj.sender = proofAgent.live  // kicksoff discussion with first text sent to human agent from sender
+      visit.isTrigger = false;
+      cb(replyObj)
+    }
+
+    if (visit.activeProof) {
+      replyObj.text = args.text;  // pass through text from sender
+      replyObj.redirect = false;
+      replyObj.callback = true;
+      replyObj.proof = false;
+      replyObj.activeProof = true; // active exchange in progress
+
+      // toggle recipients
+      if (visit.sender == args.context.saveProofSender) {
+        replyObj.sender = args.context.saveProofAgent
+      } else {
+        replyObj.sender = args.context.saveProofSender
+      }
+
+      visit.isTrigger = false;
+      cb(replyObj)
+    }
+
+    if (visit.isTrigger) {
+      replyObj.text = 'Go ahead and ask your questions. Your party is now online and ready. Enter ! to end session';
+      replyObj.redirect = false;
+      replyObj.callback = true;
+      replyObj.proof = true;     // set proof to true and being passing messages to designated agent
       visit.isTrigger = false;
       cb(replyObj)
     } else {
