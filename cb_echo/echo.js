0b0a0f8ffc82271ef53a96df54a2d542d249bba5
pdhoward <patrick.howard@hotmail.com>
2017-06-14 16:11:31 -0400

    updates

---
+++ cb_echo/echo.js
@@ -0,0 +1,77 @@
+////////////////////////////////////////////////////////////////////
+/////////          chaotic microservice                 ///////////
+////////             echo and direct                 ///////////
+//////////////////////////////////////////////////////////////////
+var greeting = require('greeting');
+const request = require('request-promise');
+
+const rp = request({
+  uri: 'http://api.forismatic.com/api/1.0/?method=getQuote&format=text&lang=en',
+  json: true,
+  headers: {'User-Agent': 'OpenWhisk'}
+})
+
+const errorResponse = [
+  'Hmmm. The quote machine is not working. Try again later',
+  'This is a surprise. I am out of quotes says the chaoticbot',
+  'Oh oh. The Quote bot seems to be offline. Sorry'
+]
+
+const triggerWords = [
+  'sale',
+  'price',
+  'pricing',
+  'training',
+  'website',
+  'product',
+  'order',
+  'buy',
+  'ship'
+]
+
+var visit = {
+  isReturn: false,
+  isKnown: false,
+  isTrigger: false,
+  count: 0
+}
+
+
+function main(args) {
+  console.log("NEW EXCHO FUNCTION")
+
+  var text = args.newMSG.text;
+
+
+  // test for trigger words which redirect to another bot (microservice)
+  var trigger = triggerWords.find(function( item ) {
+      return text.match(item);
+    });
+
+  if (trigger !== undefined) {
+      visit.isTrigger = true;
+      visit.trigger = trigger;
+  }
+
+  // compose response or redirect
+  return new Promise (function(resolve, reject){
+      respond(text, function(response) {
+          var result = {};
+          result.sender = 'echo';
+          result.orgmessage = args;
+          result.reply = response;
+          resolve(result)
+      })
+    })
+  };
+
+//respond returns a string
+function respond(text, cb) {
+
+   let reply = 'I think you said ' + text;
+   cb(reply)
+
+};
+
+
+exports.handler = main;
