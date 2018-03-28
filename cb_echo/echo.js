53a92b88ed94b496b58c9a81dd2d111af54ba427
pdhoward <patrick.howard@hotmail.com>
2018-03-28 15:43:33 -0400

    updates

--- cb_echo/echo.js
+++ cb_echo/echo.js
@@ -4,92 +4,33 @@
 //////////////////////////////////////////////////////////////////
 var greeting =              require('greeting');
 const request =             require('request-promise');
-
-const REDIS_URI='redis-15416.c12.us-east-1-4.ec2.cloud.redislabs.com'
-const REDIS_PORT= '15146'
-
-const rp = request({
-  uri: 'http://api.forismatic.com/api/1.0/?method=getQuote&format=text&lang=en',
-  json: true,
-  headers: {'User-Agent': 'OpenWhisk'}
-})
-
-const errorResponse = [
-  'Hmmm. The quote machine is not working. Try again later',
-  'This is a surprise. I am out of quotes says the chaoticbot',
-  'Oh oh. The Quote bot seems to be offline. Sorry'
-]
-
-const triggerWords = [
-  'sale',
-  'price',
-  'pricing',
-  'training',
-  'website',
-  'product',
-  'order',
-  'buy',
-  'ship'
-]
-
-var visit = {
-  isReturn: false,
-  isKnown: false,
-  isTrigger: false,
-  count: 0
-}
-
-// NOTE -- etire interact object is being passed to the microservice
-
-function main(args) {
-
-  console.log(">>>>>>>>>>>>>MEGATESTS<<<<<<<<<<<<<<<<")
-  //coming soon
+const s =                   require('serialijse')
 
 
-  console.log("NEW ECHO FUNCTION")
+function main(str) {
 
-  var text = "product";
-
-  // test for trigger words which redirect to another bot (microservice)
-  var trigger = triggerWords.find(function( item ) {
-      return text.match(item);
-    });
-
-  if (trigger !== undefined) {
-      visit.isTrigger = true;
-      visit.trigger = trigger;
-  }
+  let args = s.deserialize(args)
+  console.log("--------------micro test ---------------")
+  console.log(args.sender)
+  console.log(args.text)
+  console.log(args.sequenceCnt)
+  console.log(args.postdate)
+  console.log(args.statusInteraction)
 
   // compose response or redirect
   return new Promise (function(resolve, reject){
-      respond(args, function(response) {
           let result = {};
-          result.sender = 'echo';
+          result.sender = args.sender;
 
           // must be refactored -- part of constructor for production
           delete args.orgmessage
-
-          ////////////////////////////////////////////////////////////////////////
-          // MORE FUTURES -- DYNAMIC ROUTING
-          // name a machine and state to execute n next click or immediately before return
-          result.machine = {}
-          result.status = {}
-          result.machine.name = "testing echo "
-
-          // setting key status flags which drives routing -- on next click
-          result.status.isNewInteraction = false
-          result.status.isCallback = false
-          result.status.isTerminated = false
-          result.status.isError = false
-          result.status.isAuthenticated = false
-          result.status.isAuthorized = false
-          result.status.ErrorMsg = {msg: "no error - testing echo"}
-          //////////////////////////////////////////////////////////////////////////
-
           result.orgmessage = args;
+          ///////////////////////////////////
           result.reply = []
-          result.reply.push(response)
+          respond(args, (response) => {
+            result.reply.concat(response)
+          })
+
           let msg2 = {}
           let nextSlot = args.machine.thisSlot + 1
           if (nextSlot == args.agent.skills.length) {
@@ -101,16 +42,49 @@ function main(args) {
         //  result.reply.push({'link': 'http://www.example.com/'})
           console.log(result.reply)
           resolve(result)
-      })
+
     })
   };
 
 //respond returns a string
 function respond(args, cb) {
-  let i = args.machine.thisSlot
-   let reply = {'msg':'Agent ' + args.agent.name + ' Skill ' + args.agent.skills[i].skillname + ' Iteration ' + args.dialogue.sequenceCnt}
+  let interactions = ["Hey, I see this is your first text", "This is text number ", "Todays date is ", "You said "]
+  let response = []
+  let msg {
+    msg: ""
+  }
 
-   cb(reply)
+  let t = args.sequenceCnt
+
+  switch(t) {
+    case 0:
+      msg.msg = interactions[1] + t + " which seems a little low"
+      response.push(msg)
+
+      break;
+    case 1:
+      msg.msg = interactions[2] + args.postdate
+      response.push(msg)
+
+      break;
+    case 2:
+      msg.msg = interactions[3] + args.text
+      response.push(msg)
+      msg.msg = "Which is brilliant"
+      response.push(msg)
+
+      break;
+
+    default:
+      msg.msg = interactions[1] + t
+      response.push(msg)
+      msg.msg = "When will this discussion end"
+      response.push(msg)
+
+      break;
+  }
+
+   cb(response)
 
 };
 
