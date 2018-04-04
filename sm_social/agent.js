7da001b3e26fc440cce5644263004e7fe084d81e
pdhoward <patrick.howard@hotmail.com>
2018-04-04 13:18:41 -0400

    updates

--- sm_social/agent.js
+++ sm_social/agent.js
@@ -55,12 +55,7 @@ function main(obj) {
 
 //respond returns a string
 function respond(args, cb) {
-  let interactions = ["Hey, great to hear from you ",
-                      "This is text number ",
-                      "Todays date is ",
-                      "You said ",
-                      "I am here to sell you a toy. If you need to speak to a live agent, just say so",
-                      "Can I interest you in a toy? "]
+  let interactions = ["Thank you! You are so kind!"]
 
   let response = []
   let msg = {
@@ -70,53 +65,13 @@ function respond(args, cb) {
 
   let t = args.sequenceCnt
   let v = args.obj.dialogue.sequenceCnt
-  switch(t) {
-    case 0:
-      msg.msg = interactions[1] + t + " which seems a little low"
-      response.push(msg)
-      cb(response)
-      break;
-    case 1:
-      msg.msg = interactions[0] + args.senderName
-      newObj = Object.assign({}, msg)
-      response.push(newObj)
-      msg.msg = interactions[4]
-      newObj = Object.assign({}, msg)
-      response.push(newObj)
-      cb(response)
-      break;
-    case 2:
-      msg.msg = interactions[3] + args.text
-      newObj = Object.assign({}, msg)
-      response.push(newObj)
-      msg.msg = interactions[5]
-      newObj = Object.assign({}, msg)
-      response.push(newObj)
-      cb(response)
-      break;
-    //
-    case 3:
-      msg.msg = interactions[1] + t + " which is a lot of texting"
-      response.push(msg)
-      cb(response)
-      break;
-    //
-    case 4:
-      msg.msg = interactions[2] + args.postdate
-      response.push(msg)
-      cb(response)
-      break;
 
-    default:
-      msg.msg = interactions[1] + t
-      newObj = Object.assign({}, msg)
-      response.push(newObj)
-      msg.msg = "When will this discussion end?"
-      newObj = Object.assign({}, msg)
-      response.push(newObj)
-      cb(response)
-      break;
-  }
+  msg.msg = interactions[0]
+  newObj = Object.assign({}, msg)
+  response.push(newObj)
+
+  cb(response)
+
 
 };
 //respond returns a string
